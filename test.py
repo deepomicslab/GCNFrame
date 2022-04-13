e164f8af22f0a53bc25218d24d5138a2385722d5
WANG Ruohan <42761637+wangruohan111@users.noreply.github.com>
2022-04-14 00:20:57 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from GNNFrame import Biodata, GNNmodel
-import torch
-
-data = Biodata.Biodata(fasta_file="/home/ruohawang2/dataset/Phage_nature2017/nature_2017.fasta", label_file="/home/ruohawang2/dataset/Phage_nature2017/kmer_encoding/lifestyle_label.txt", feature_file="/home/ruohawang2/06.GNN_framework/phage_lifestyle/dataset/nature_2017/CDD_feature/CDD_protein_feature.txt")
-dataset = data.encode(thread=20)
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = GNNmodel.model(label_num=2, other_feature_dim=206).to(device)
-GNNmodel.train(dataset, model, weighted_sampling=False)
